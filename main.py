9da7963ae50ef120afe44399fd20b7bb4f902f30
Savannah Rimmele <rimmeles@gmail.com>
2022-06-13 18:16:52 -0400

    Created calculateF function
    Co-authored-by: Joseph Garro <jgarr0@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # lokup for converting hex to 4 digit binary
-from re import I
+# xor two strings
+def xor(str1, str2):
+    # empty result string
+    res = ""
+    if(len(str1) == len(str2)):
+        for i in range(0, len(str1)):
+            if(str1[i] == '0' and str2[i] == '0'):
+                res += '0'
+            elif(str1[i] == '1' and str2[i] == '1'):
+                res += '0'
+            else:
+                res += "1"
+        return res
 
+    else:
+        return 0
 
 HEXDICT = {
     '0' : '0000',
@@ -60,7 +74,7 @@ IP  =  [58, 50, 42, 34, 26, 18, 10, 2,
         61, 53, 45, 37, 29, 21, 13, 5,
         63, 55, 47, 39, 31, 23, 15, 7]
 
-# Exansion table from 32 bit input to 48 bit output
+# Expansion table from 32 bit input to 48 bit output
 E = [32, 1, 2, 3, 4, 5, 
      4, 5, 6, 7, 8, 9, 
      8, 9, 10, 11, 12, 13, 
@@ -69,6 +83,57 @@ E = [32, 1, 2, 3, 4, 5,
      20, 21, 22, 23, 24, 25, 
      24, 25, 26, 27, 28, 29, 
      28, 29, 30, 31, 32, 1]
+
+# substitution boxes
+S1 = [14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7,
+       0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8,
+       4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0,
+      15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
+
+S2 = [15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10,
+       3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5,
+       0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15,
+      13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
+
+S3 = [10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8,
+      13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1,
+      13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7,
+       1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
+
+S4 = [7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15,
+     13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9,
+     10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4,
+      3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
+
+S5 = [2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9,
+     14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6,
+      4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14,
+     11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
+
+S6 = [12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11,
+      10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8,
+       9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6,
+       4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
+
+S7 = [4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1,
+     13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6,
+      1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2,
+      6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
+
+S8 = [13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7,
+       1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2,
+       7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8,
+       2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+
+# Permutation to calculate f
+P = [16, 7, 20, 21, 
+     29, 12, 28, 17, 
+     1, 15, 23, 26, 
+     5, 18, 31, 10, 
+     2, 8, 24, 14, 
+     32, 27, 3, 9, 
+     19, 13, 30, 6, 
+     22, 11, 4, 25]
 
 # assume input string has already been converted into hex
 def str2binary(inputString):
@@ -91,7 +156,8 @@ def permutation(inputString, dict):
     permutation = ""
     for i in range(0, len(dict)):
         # subtract 1 from index as python is 0 indexed
-        permutation += inputString[dict[i]-1]
+        permutation += inputString[(dict[i]-1)]
+        #print(permutation)
 
     return permutation
 
@@ -136,30 +202,39 @@ def genCD(inputString):
 
 def formKeyArray(inputArray):
     key_array = [0] * 17
-    # fill k0
-    key_array[0] = inputArray[0] + inputArray[1]
 
     #fill kn
-    for i in range(2, 33, 2):
+    for i in range(2, 34, 2):
         temp = inputArray[i] + inputArray[i+1]
-        key_array[int(i/2)] = permutation(temp, PC2)
-
+        key_array[int(i/2)-1] = permutation(temp, PC2)
     return key_array
 
-# #Form PermutedMsg into left and right halves 
-# def genLR(permutedMsg, key): 
-#     # Create empty 34x32 array for LnRn pairs.  
-#     LR_array = [0]*34; 
+def calculateF(rString, keyString):
+    # compute expansion of Rn-1
+    eString = permutation(rString, E)
+    # compute the key xor e
+    kxe = xor(eString, keyString)
+    # for i in range(0, len(kxe)): 
+    #     sbox = 
+    # result = permutation(kxe)
+    # print("Result: " + result)
+    # return result
 
-#     # Initialize L0 and R0
-#     LR_array[0] = permutedMsg[0:32]
-#     LR_array[1] = permutedMsg[32:64]
+#Form PermutedMsg into left and right halves 
+def genLR(permutedMsg, keyArray): 
+    # Create empty 34x32 array for LnRn pairs. 
+    LR_array = [0]*34
 
-#     # Initialize remaining Ln and Rn
-#     for i in range(2, 33, 2): 
-#         Ln = LR_array[i - 1]; 
-#         Rn = Ln[i-1] 
+    # Initialize L0 and R0
+    LR_array[0] = permutedMsg[0:32]         # l0
+    LR_array[1] = permutedMsg[32:64]        # r0
 
+    # Initialize remaining Ln and Rn
+    for i in range(2, 34, 2): 
+        Ln = LR_array[i - 1]                # ln = Rn-1
+        f_result = calculateF(LR_array[i-1], keyArray[int(i/2)-1])
+        print("F FORMED")
+        
 
     
 
@@ -206,8 +281,8 @@ for i in range(2, 34, 2):
 key_array = formKeyArray(key_parts)
 
 # check compared to provided keys
-known_key_aray = ["000110110000001011101111111111000111000001110010",
-                  "011110011010111011011001110110111100100111100101",
+known_key_aray = ["000110110000001011101111111111000111000001110010", 
+                  "011110011010111011011001110110111100100111100101", 
                   "010101011111110010001010010000101100111110011001",
                   "011100101010110111010110110110110011010100011101",
                   "011111001110110000000111111010110101001110101000",
@@ -216,17 +291,16 @@ known_key_aray = ["000110110000001011101111111111000111000001110010",
                   "111101111000101000111010110000010011101111111011",
                   "111000001101101111101011111011011110011110000001",
                   "101100011111001101000111101110100100011001001111",
-                  "001000010101111111010011110111101101001110000110",
+                  "001000010101111111010011110111101101001110000110", 
                   "011101010111000111110101100101000110011111101001",
                   "100101111100010111010001111110101011101001000001",
                   "010111110100001110110111111100101110011100111010",
                   "101111111001000110001101001111010011111100001010",
-                  "110010110011110110001011000011100001011111110101"]
-
+                  "110010110011110110001011000011100001011111110101"] 
 print('\n')
 
 for i in range(0, 16):
-    if(known_key_aray[i] == key_array[i+1]):
+    if(known_key_aray[i] == key_array[i]):
         print("KEY", int(i+1), " MATCH")
     else:
          print("KEY", int(i+1), " ERROR")
@@ -235,3 +309,4 @@ print('\n')
 permutedMsg = permutation(message_binary, IP)
 print(permutedMsg)
 
+genLR(permutedMsg, key_array)
